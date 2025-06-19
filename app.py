ce694514fea4463486dba1e8ebf329168ef2e82c
Pragatheesh R <113919789+pragatheeshuidev@users.noreply.github.com>
2025-06-20 00:29:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import os
 from AI_Resume_Optimizer.utils.resume_parser import parse_resume
 from AI_Resume_Optimizer.job_description_cleaner import clean_job_description
-from AI_Resume_Optimizer.similarity import calculate_similarity
+from utils.similarity import calculate_similarity
 from AI_Resume_Optimizer.utils.resume_parser import parse_resume
-
-
+from utils.resume_scorer import score_resume
+from utils.feedback_generator import generate_feedback
 from job_description_cleaner import clean_job_description
 from similarity import calculate_similarity
 from resume_scorer import score_resume
